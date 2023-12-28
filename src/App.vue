d7f27669e230fdde54ace560550b0d291bc4ee5e
TheShyen <ruslankaslesh@mail.ru>
2023-12-28 13:01:55 +0500

    commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {onMounted, reactive, ref, watch} from 'vue'
+import Input from "./components/Input.vue";
+
+onMounted(() => {
+  console.log('mounted')
+})
+
+
+const title = ref('v-model argument example')
+const obj = reactive({click: createObj})
+
+watch(() => obj.click, () => {
+  console.log('watcher')
+}, {immediate: true})
+function createObj() {
+  console.log(obj)
+  delete obj.click
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <input
+    type="text"
+    v-model="title"
+  />
+  <button v-on="obj">fff</button>
+</template>
